b8baa18693d00fc626ce7839fe363d51b0b43645
shoebby <59800629+shoebby@users.noreply.github.com>
2021-05-25 10:34:24 +0200

    levels en level reset
    ADDED:
    - level 1 t/m 5 blockouts
    - level transitioner prefab (portal naar volgend level)
    CHANGED:
    REMOVED:

---
+++ visTest/Assets/SceneTransitioner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitioner : MonoBehaviour
+{
+    private Collider coll;
+    public string nextScene;
+    public string currentScene;
+
+    void Start()
+    {
+        coll = GetComponent<BoxCollider>();
+        currentScene = SceneManager.GetActiveScene().name;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(currentScene);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
--- visTest/Assets/Scripts/UI_Bars.cs
+++ visTest/Assets/Scripts/UI_Bars.cs
@@ -31,29 +31,35 @@ public class UI_Bars : MonoBehaviour
     private void Update()
     {
         grenades_Left_Text.text = grenades_left.ToString();
-           
-        if(cur_value != max_value)
+        
+        //If player uses grenade, the image will go to value 0 and one number will dissapear
+        if (Input.GetKeyDown(mechanic) && grenades_left >= 1f)
+        {
+            grenades_left -= grenades_gifted;
+            if (!charging)
+            {
+                cur_value = 0f;
+
+                InvokeRepeating("increasePowerUp", 0f, .5f); //name, time, repeatrate
+            }
+            //Debug.Log(grenades_left);
+        }
+
+        if (cur_value < max_value)
         {
             charging = true;
-        }
-
-        //If player uses grenade, the image will go to value 0 and one number will dissapear
-        if (Input.GetKeyDown(mechanic) && !charging)
+        } else if (cur_value >= max_value)
         {
-            grenades_left = grenades_left - grenades_gifted;
-            cur_value = 0f;
-
-            InvokeRepeating("increasePowerUp", 0f, .5f); //name, time, repeatrate
-
+            charging = false;
         }
 
-        if(!charging && grenades_left != max_grenades)
+        if (!charging && grenades_left < max_grenades)
         {
-            grenades_left = grenades_left + grenades_gifted;
+            grenades_left += grenades_gifted;
         }
-        
-        Debug.Log(grenades_left);
-        Debug.Log(charging);
+
+        //Debug.Log(grenades_left);
+        //Debug.Log(charging);
     }
 
     //Image will start filling after
@@ -61,6 +67,7 @@ public class UI_Bars : MonoBehaviour
     {
         cur_value += fillSpeed;
         Bar.fillAmount = (float)cur_value / (float)max_value;
+        Debug.Log(Bar.fillAmount);
     }
 
     //If the bar is filled, a new grenade will be usable
