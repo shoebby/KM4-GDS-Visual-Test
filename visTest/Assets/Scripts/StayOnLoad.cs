aa06208833794931602d00e04f980d4a0ee112d6
RoryPumpkin <rowinvanderschee@gmail.com>
2021-05-31 21:13:09 +0200

    Moved some shrooms

--- visTest/Assets/Scripts/StayOnLoad.cs
+++ visTest/Assets/Scripts/StayOnLoad.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StayOnLoad : MonoBehaviour
 {
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().name == "")
+        {
+
+        }
     }
 }
